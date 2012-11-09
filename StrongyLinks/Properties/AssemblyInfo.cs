93940ce63f41966d1df6f77fdea2ea91fb73b427
odinserj <odinserj@chessok.com>
2012-11-10 00:49:48 +0400

    Fixed #1: Automatic registration of `ChessOk.StrongyLinks` namespace

--- StrongyLinks/Properties/AssemblyInfo.cs
+++ StrongyLinks/Properties/AssemblyInfo.cs
@@ -10,5 +10,5 @@ using System.Runtime.InteropServices;
  
 [assembly: Guid("5097aaa9-22d3-4da8-a834-e792a401d392")]
  
-[assembly: AssemblyVersion("1.0.0.*")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.1.*")]
+[assembly: AssemblyInformationalVersion("1.0.1")]
